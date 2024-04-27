be5b82f7b534edc3b5d8f90da7162dea8a45e817
Lee88ham <164107131+Lee88ham@users.noreply.github.com>
2024-04-27 12:34:03 -0500

    Add files via upload

---
+++ projects_database_week8.sql
@@ -0,0 +1,48 @@
+DROP TABLE IF EXISTS project_category;
+DROP TABLE IF EXISTS category;
+DROP TABLE IF EXISTS step;
+DROP TABLE IF EXISTS material;
+DROP TABLE IF EXISTS project;
+
+CREATE TABLE project (
+project_id INT AUTO_INCREMENT NOT NULL,
+project_name VARCHAR(128) NOT NULL,
+estimated_hours DECIMAL(7,2),
+actual_hours DECIMAL(7,2),
+difficulty INT,
+notes TEXT,
+primary key (project_id)
+);
+
+CREATE TABLE material (
+material_id INT AUTO_INCREMENT NOT NULL,
+project_id INT NOT NULL,
+material_name VARCHAR(128) NOT NULL,
+num_required INT,
+cost DECIMAL(7,2),
+primary key (material_id),
+foreign key (project_id) references project (project_id) on delete CASCADE
+);
+
+CREATE TABLE  step (
+step_id INT AUTO_INCREMENT NOT NULL,
+project_id INT NOT NULL,
+step_text TEXT NOT NULL,
+step_order INT NOT null,
+primary key (step_id),
+foreign key (project_id) references project (project_id) on delete CASCADE
+);
+
+CREATE TABLE category (
+category_id INT AUTO_INCREMENT NOT NULL,
+category_name VARCHAR(128),
+primary key (category_id)
+);
+
+CREATE TABLE project_category (
+project_id INT NOT NULL,
+category_id INT NOT null,
+foreign key (project_id) references project (project_id) on delete cascade, 
+foreign key (category_id) references category (category_id) on delete cascade,
+unique key (project_id, category_id)
+);
